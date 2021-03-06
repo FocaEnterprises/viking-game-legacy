aed8b96d6738d56fe7a7d7d02eddd04df4db6352
GiverPlay007 <play.giver.007@gmail.com>
2021-03-05 21:28:35 -0300

    Initial commit

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+    [SerializeField]
+    float movementSpeed = 4.0f;
+    [SerializeField]
+    float rotationSpeed = 4.0f;
+
+    float velocity = .0f;
+    float rotation = .0f;
+
+    static Transform s_selfTransform;
+
+    void Start() {
+        s_selfTransform = this.GetComponent<Transform>();
+    }
+
+    void Update() {
+        rotation = Input.GetAxis("Horizontal");
+        velocity = Input.GetAxis("Vertical");
+    }
+
+    /* roda sempre em 60 fps */
+    void FixedUpdate() {
+        s_selfTransform.Translate(Vector3.forward * velocity * movementSpeed * Time.fixedDeltaTime, Space.Self);
+        s_selfTransform.Rotate(new Vector3(.0f, rotation * rotationSpeed, .0f), Space.Self);
+    }
+}
